16fc6b7696d0a0b530ead1c9cb44762d33ec6264
Fernando Lage Novaes <108960402+malakpentest@users.noreply.github.com>
2024-01-08 11:19:29 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
-﻿using DesafioPOO.Models;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+// DONE
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Nokia nk123 = new Nokia("000145", "NK123", "305460516516051651", 256);
+        nk123.InstalarAplicativo("WhatsApp");
+    }
+}
