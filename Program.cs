b1d0631d946b37775a660f11bc0d72b539f75390
Fernando Lage Novaes <108960402+malakpentest@users.noreply.github.com>
2024-01-08 11:26:31 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,14 +3,14 @@
 // TODO: Realizar os testes com as classes Nokia e Iphone
 
 Console.WriteLine("Smartphone Nokia: ");
-Nokia nokia = new Nokia("123", "1111111111", "Modelo 1", 32);
-nokia.InstalarAplicativo("Whatassp");
+Nokia nokia = new Nokia("210685462", "123456798", "Modelo A", 11);
+nokia.InstalarAplicativo("Dio.me");
 nokia.Ligar();
 
 
 Console.WriteLine("\n");
 
 Console.WriteLine("Smartphone iPhone:");
-Iphone iphone = new Iphone("123", "22222222", "Modelo 2", 16);
-iphone.InstalarAplicativo("Whatassp");
+Iphone iphone = new Iphone("123", "22222222", "Modelo b", 22);
+iphone.InstalarAplicativo("WS");
 iphone.ReceberLigacao();
